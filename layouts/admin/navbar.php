818355a6507d2810fb10fbaebfbe9655527977e7
MirrorBottle <setiawanbayu66152@gmail.com>
2022-10-11 11:09:33 +0800

    fix: logo

--- layouts/admin/navbar.php
+++ layouts/admin/navbar.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none">
-  <a class="navbar-brand me-lg-5" href="../../assets/dashboard/index.html">
-    <img class="navbar-brand-dark" src="../../assets/img/logo.png" alt="Volt logo" /> 
-    <img class="navbar-brand-light" src="../../assets/img/logo.png" alt="Volt logo" />
+  <a class="navbar-brand me-lg-5 text-white" href="#">
+    <h1>Galeri Gitar</h1>
   </a>
   <div class="d-flex align-items-center">
     <button class="navbar-toggler d-lg-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
