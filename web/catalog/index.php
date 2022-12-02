a4642802a9130b907d4976229d8c7fc5e793c48c
MirrorBottle <setiawanbayu66152@gmail.com>
2022-12-02 17:13:18 +0800

    feat: catalog

--- web/catalog/index.php
+++ web/catalog/index.php
@@ -1,201 +1,36 @@
 <?php require_once('../../layouts/web/header.php') ?>
+
+<?php include("../../core/functions.php"); ?>
+
+<?php $guitars = all("gitar") ?>
 <!-- MAIN -->
 <div class="site-section">
     <div class="container">
 
         <div class="row mb-5">
-            <div class="col-md-9 order-2">
-
-                <div class="row">
-                    <div class="col-md-12 mb-5">
-                        <div class="float-md-left mb-4">
-                            <h2 class="text-black h5">Shop All</h2>
-                        </div>
-                        <div class="d-flex">
-                            <div class="dropdown mr-1 ml-md-auto">
-                                <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Latest
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                    <a class="dropdown-item" href="#">Men</a>
-                                    <a class="dropdown-item" href="#">Women</a>
-                                    <a class="dropdown-item" href="#">Children</a>
-                                </div>
-                            </div>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-secondary btn-sm dropdown-toggle" id="dropdownMenuReference" data-toggle="dropdown">Reference</button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuReference">
-                                    <a class="dropdown-item" href="#">Relevance</a>
-                                    <a class="dropdown-item" href="#">Name, A to Z</a>
-                                    <a class="dropdown-item" href="#">Name, Z to A</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Price, low to high</a>
-                                    <a class="dropdown-item" href="#">Price, high to low</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-md-12 order-2">
                 <div class="row mb-5">
+                    <?php foreach ($guitars as $guitar) : ?>
+                        <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+                            <div class="block-4 text-center border">
+                                <figure class="block-4-image">
+                                    <a href="shop-single.html">
+                                        <img src="<?= $guitar['gambar'] ?>" alt="Image placeholder" class="img-fluid">
+                                    </a>
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><?= $guitar['nama'] ?></h3>
+                                    <h5><?= $guitar['tipe'] ?></h5>
+                                    <p class="mb-0">
+                                        <?= $guitar['deskripsi'] ?>
+                                    </p>
+                                    <p class="text-primary font-weight-bold">Rp. <?= number_format($guitar['harga']) ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
 
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1519640350407-953bc0614f4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik</a></h3>
-                                <p class="mb-0">Simple, terbaik</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1618530089935-3030738b8c7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Bass</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1531907933630-126f527982d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik II</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1622584968804-58fc2a78cfc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=739&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Elektrik</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1618530089935-3030738b8c7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Bass</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1519640350407-953bc0614f4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik</a></h3>
-                                <p class="mb-0">Simple, terbaik</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1618530089935-3030738b8c7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Bass</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1531907933630-126f527982d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik II</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1622584968804-58fc2a78cfc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=739&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Elektrik</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1618530089935-3030738b8c7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Bass</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1519640350407-953bc0614f4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik</a></h3>
-                                <p class="mb-0">Simple, terbaik</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                        <div class="block-4 text-center border">
-                            <figure class="block-4-image">
-                                <a href="shop-single.html"><img src="https://images.unsplash.com/photo-1531907933630-126f527982d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Image placeholder" class="img-fluid"></a>
-                            </figure>
-                            <div class="block-4-text p-4">
-                                <h3><a href="shop-single.html">Akustik II</a></h3>
-                                <p class="mb-0">Go Rock!</p>
-                                <p class="text-primary font-weight-bold">Rp. 2.500.000</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 order-1 mb-5 mb-md-0">
-                <div class="border p-4 rounded mb-4">
-                    <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
-                    <ul class="list-unstyled mb-0">
-                        <li class="mb-1"><a href="#" class="d-flex"><span>Men</span> <span class="text-black ml-auto">(2,220)</span></a></li>
-                        <li class="mb-1"><a href="#" class="d-flex"><span>Women</span> <span class="text-black ml-auto">(2,550)</span></a></li>
-                        <li class="mb-1"><a href="#" class="d-flex"><span>Children</span> <span class="text-black ml-auto">(2,124)</span></a></li>
-                    </ul>
                 </div>
             </div>
         </div>
